c8b1239eeecf421d946b6e64b0829eae1356cfb7
Stanislav Pankrashin <stasera2@gmail.com>
2016-09-05 00:57:00 +1200

    Finished currencyConverter logic.
    Still need to make page pretty.

--- currencyConversion/Javascript/currencyConversion.js
+++ currencyConversion/Javascript/currencyConversion.js
@@ -5,51 +5,101 @@ var CurrencyConverter = (function () {
     CurrencyConverter.prototype.CurrencyConverter = function () {
     };
     CurrencyConverter.prototype.setCurrencies = function (currencyFrom, currencyTo) {
-        this.currencyFrom = currencyFrom;
-        this.currencyTo = currencyTo;
+        if (this.checkCurrencies(currencyFrom, currencyTo)) {
+            this.currencyFrom = currencyFrom;
+            this.currencyTo = currencyTo;
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    CurrencyConverter.prototype.checkCurrencies = function (currencyFrom, currencyTo) {
+        var currencyFromValid;
+        var currencyToValid;
+        var currencies = ["AUD", "BGN", "BRL", "CAD",
+            "CHF", "CNY", "CZK", "DKK",
+            "GBP", "HKD", "HRK", "HUF",
+            "IDR", "ILS", "INR", "JPY",
+            "KRW", "MXN", "MYR", "NOK",
+            "NZD", "PHP", "PLN", "RON",
+            "RUB", "SEK", "SGD", "THB",
+            "TRY", "USD", "ZAR"];
+        if (currencies.indexOf(currencyFrom) != -1) {
+            currencyFromValid = true;
+        }
+        else {
+            currencyFromValid = false;
+        }
+        if (currencies.indexOf(currencyTo) != -1) {
+            currencyToValid = true;
+        }
+        else {
+            currencyToValid = false;
+        }
+        return currencyFromValid && currencyToValid;
     };
     CurrencyConverter.prototype.setAmount = function (amount) {
         this.currencyAmount = amount;
     };
     CurrencyConverter.prototype.getRates = function (callback) {
         var settings = {
-            async: false,
+            async: true,
             crossDomain: true,
-            url: "http://api.fixer.io/latest?symbols=USD%2CNZD",
+            url: "http://api.fixer.io/latest?symbols={1}%2C{2}".replace("{1}", this.currencyFrom).replace("{2}", this.currencyTo),
             method: "GET"
         };
         $.ajax(settings).done(function (response) {
             callback(response);
+        }).fail(function (response) {
+            this.failMessage();
         });
-    };
-    CurrencyConverter.prototype.getCallBack = function () {
-        var result;
-        this.getRates(function (data) {
-            result = data;
-        });
-        return result;
     };
     CurrencyConverter.prototype.calculate = function (data) {
         if (!this.giveError) {
             this.currencyFromValue = Number(data["rates"][this.currencyFrom]);
             this.currencyToValue = Number(data["rates"][this.currencyTo]);
             this.currencyRatio = this.currencyToValue / this.currencyFromValue;
-            this.finalResult = Number(this.currencyAmount) * this.currencyRatio;
+            this.finalResult = String((Number(this.currencyAmount) * this.currencyRatio).toFixed(2));
         }
     };
     CurrencyConverter.prototype.returnResult = function () {
-        $("#resultBtn").text(this.finalResult);
+        var message;
+        message = "{0} {1} in {2} is {3}"
+            .replace("{0}", this.currencyAmount)
+            .replace("{1}", this.currencyFrom)
+            .replace("{2}", this.currencyTo)
+            .replace("{3}", this.finalResult);
+        $("#resultBtn").text(message);
+    };
+    CurrencyConverter.prototype.failMessage = function () {
+        var message;
+        message = "One or more of the currency Codes is Incorrect";
+        $("#resultBtn").text(message);
     };
     return CurrencyConverter;
 }());
 function convertCurrency() {
-    var currencyFrom = $("#currencyFrom").val();
-    var currencyTo = $("#currencyTo").val();
+    var currencyFrom = $("#currencyFrom").val().toUpperCase();
+    var currencyTo = $("#currencyTo").val().toUpperCase();
     var currencyAmount = $("#currencyAmount").val();
     var currencyConverter = new CurrencyConverter();
-    currencyConverter.setCurrencies(currencyFrom, currencyTo);
+    var incorrectCurrencies = currencyConverter.setCurrencies(currencyFrom, currencyTo);
+    if (!incorrectCurrencies) {
+        currencyConverter.failMessage();
+        return;
+    }
     currencyConverter.setAmount(currencyAmount);
-    var returnedData = currencyConverter.getCallBack();
-    currencyConverter.calculate(returnedData);
-    currencyConverter.returnResult();
+    currencyConverter.getRates(function (data) {
+        currencyConverter.calculate(data);
+        currencyConverter.returnResult();
+    });
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function showCurrencies() {
+    swal({
+        title: "Available currency codes:",
+        text: "AUD: Australian Dollar\nBGN: Bulgarian Vel\nBRL: Brazilian Real\nCAD: Canadian Dollar\nCHF: Swiss Franc\nCNY: Chinese Yuan\nCZK: Czech Koruna\nDKK: Danish Krone\nGBP: British Pound\nHKD: Hong Kong Dollar\nHRK: Croatian Kuna\nHUF: Hungarian Forint\nIDR: Indonesian Rupiah\nILS: Israeli New Sheqel\nINR: Indian Rupee\nJPY: Japanese Yen\nKRW: South Korean Won\nMXN: Mexican Peso\nMYR: Malaysian Ringgit\nNOK: Norwegian Krone\nNZD: New Zealand Dollar\nPHP: Philippine Peso\nPLN: Polish Zloty\nRON: Romanian Leu\nRUB: Russian Ruble\nSEK: Swedish Krone\nSGD: Singapore Dollar\nTHB: Thai Baht\nTRY: Turkish Lira\nUSD: United States Dollar\nZAR: South African Rand",
+        type: "info",
+        confirmButtonText: "Return" });
 }
--- currencyConversion/typescript/currencyConversion.ts
+++ currencyConversion/typescript/currencyConversion.ts
@@ -4,21 +4,50 @@ class CurrencyConverter {
     currencyAmount: string;
 
     giveError: boolean = false;
-    jsonData: JSON;
     currencyFromValue: number;
     currencyToValue: number;
     currencyRatio: number;
 
-    finalResult: number;
+    finalResult: string;
 
     CurrencyConverter() {
 
     }
 
-    setCurrencies(currencyFrom: string, currencyTo: string): void {
-        this.currencyFrom = currencyFrom;
-        this.currencyTo = currencyTo;
+    setCurrencies(currencyFrom: string, currencyTo: string): boolean {
+        if (this.checkCurrencies(currencyFrom, currencyTo)){
+            this.currencyFrom = currencyFrom;
+            this.currencyTo = currencyTo;
+            return true
+        }else{
+            return false
+        }
 
+    }
+
+    checkCurrencies(currencyFrom: string, currencyTo: string): boolean{
+        var currencyFromValid: boolean;
+        var currencyToValid: boolean;
+        var currencies: string[] = ["AUD","BGN","BRL", "CAD",
+                                    "CHF","CNY","CZK","DKK",
+                                    "GBP","HKD","HRK","HUF",
+                                    "IDR","ILS","INR","JPY",
+                                    "KRW","MXN","MYR","NOK",
+                                    "NZD","PHP","PLN","RON",
+                                    "RUB","SEK","SGD","THB",
+                                    "TRY","USD","ZAR"]; 
+        if (currencies.indexOf(currencyFrom) !=-1){
+            currencyFromValid = true;
+        }else{
+            currencyFromValid = false;
+        }
+        if (currencies.indexOf(currencyTo) !=-1){
+            currencyToValid = true;
+        }else{
+            currencyToValid = false;
+        }
+
+        return currencyFromValid && currencyToValid
     }
 
     setAmount(amount: string): void {
@@ -26,56 +55,77 @@ class CurrencyConverter {
 
     }
 
-    getRates(callback): any {
+    getRates(callback): void {
         var settings = {
-            async: false,
+            async: true,
             crossDomain: true,
-            url: "http://api.fixer.io/latest?symbols=USD%2CNZD",
+            url: "http://api.fixer.io/latest?symbols={1}%2C{2}".replace("{1}", this.currencyFrom).replace("{2}", this.currencyTo),
             method: "GET",
         }
 
         $.ajax(settings).done(function (response) {
             callback(response);
+        }).fail(function(response){
+            this.failMessage();
         });
 
     }
-
-    getCallBack(): any {
-        var result: any;
-        this.getRates(function (data) {
-            result = data;
-        });
-        return result;
-    }
-
 
     calculate(data: JSON): void {
         if (!this.giveError) {
             this.currencyFromValue = Number(data["rates"][this.currencyFrom]);
             this.currencyToValue = Number(data["rates"][this.currencyTo]);
             this.currencyRatio = this.currencyToValue / this.currencyFromValue;
-            this.finalResult = Number(this.currencyAmount) * this.currencyRatio;
+            this.finalResult = String(
+                (Number(this.currencyAmount) * this.currencyRatio).toFixed(2));
         }
 
     }
     returnResult(): void {
-        $("#resultBtn").text(this.finalResult);
+        var message: string;
+        message = "{0} {1} in {2} is {3}"
+            .replace("{0}", this.currencyAmount)
+            .replace("{1}", this.currencyFrom)
+            .replace("{2}", this.currencyTo)
+            .replace("{3}", this.finalResult);
 
+        $("#resultBtn").text(message);
+
+    }
+    failMessage(): void{
+       var message: string;
+       message = "One or more of the currency Codes is Incorrect"
+       $("#resultBtn").text(message); 
     }
 
 
 }
 
 function convertCurrency(): void {
-    var currencyFrom: string = $("#currencyFrom").val();
-    var currencyTo: string = $("#currencyTo").val();
+    var currencyFrom: string = $("#currencyFrom").val().toUpperCase();
+    var currencyTo: string = $("#currencyTo").val().toUpperCase();
     var currencyAmount: string = $("#currencyAmount").val();
-
     var currencyConverter = new CurrencyConverter();
-    currencyConverter.setCurrencies(currencyFrom, currencyTo);
+    var incorrectCurrencies: boolean = currencyConverter.setCurrencies(currencyFrom, currencyTo);
+    if (!incorrectCurrencies){
+        currencyConverter.failMessage();
+        return
+    }
+    
+    
     currencyConverter.setAmount(currencyAmount);
-    let returnedData: any = currencyConverter.getCallBack();
+    currencyConverter.getRates(function (data) {
+        currencyConverter.calculate(data);
+        currencyConverter.returnResult();
+    });
+}
 
-    currencyConverter.calculate(returnedData);
-    currencyConverter.returnResult();
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function showCurrencies(): void {
+    swal({ 
+        title: "Available currency codes:", 
+        text: "AUD: Australian Dollar\nBGN: Bulgarian Vel\nBRL: Brazilian Real\nCAD: Canadian Dollar\nCHF: Swiss Franc\nCNY: Chinese Yuan\nCZK: Czech Koruna\nDKK: Danish Krone\nGBP: British Pound\nHKD: Hong Kong Dollar\nHRK: Croatian Kuna\nHUF: Hungarian Forint\nIDR: Indonesian Rupiah\nILS: Israeli New Sheqel\nINR: Indian Rupee\nJPY: Japanese Yen\nKRW: South Korean Won\nMXN: Mexican Peso\nMYR: Malaysian Ringgit\nNOK: Norwegian Krone\nNZD: New Zealand Dollar\nPHP: Philippine Peso\nPLN: Polish Zloty\nRON: Romanian Leu\nRUB: Russian Ruble\nSEK: Swedish Krone\nSGD: Singapore Dollar\nTHB: Thai Baht\nTRY: Turkish Lira\nUSD: United States Dollar\nZAR: South African Rand",
+        type: "info", 
+        confirmButtonText: "Return" });
 }
