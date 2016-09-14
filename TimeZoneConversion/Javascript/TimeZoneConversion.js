b5417798d4f628472eed6438632f6417bf45aaa8
Stanislav Pankrashin <stasera2@gmail.com>
2016-09-14 23:43:13 +1200

    Submit for testing of timezoneconversion scripts

--- TimeZoneConversion/Javascript/TimeZoneConversion.js
+++ TimeZoneConversion/Javascript/TimeZoneConversion.js
@@ -1,38 +1,20 @@
-class TimeZoneConverter {
-    cityFrom: string;
-    cityTo: string;
-    originTime: string;
-
-    cityFromLat: string;
-    cityFromLong: string;
-    cityToLat: string;
-    cityToLong: string;
-
-    cityFromTimezone: string;
-    cityToTimezone: string;
-
-    convertedTime: number;
-
-
-    TimeZoneConverter(): void { }
-
-    setCities(cityFrom: string, cityTo: string) {
+var TimeZoneConverter = (function () {
+    function TimeZoneConverter() {
+    }
+    TimeZoneConverter.prototype.TimeZoneConverter = function () { };
+    TimeZoneConverter.prototype.setCities = function (cityFrom, cityTo) {
         this.cityFrom = cityFrom;
         this.cityTo = cityTo;
-    }
-
-    setOriginTime(originTime: string): void {
+    };
+    TimeZoneConverter.prototype.setOriginTime = function (originTime) {
         this.originTime = originTime;
-    }
-
-    retrieveInformation(): void {
+    };
+    TimeZoneConverter.prototype.retrieveInformation = function () {
         //main method where api calls will be chained
         this.getCoordinatesCityFrom();
-
-    }
-
+    };
     //The next two methods can be executed concurrently, but shouldnt be due to limitations with calculation and multithreading. will need to chain all ajax calls
-    getCoordinatesCityFrom(): void {
+    TimeZoneConverter.prototype.getCoordinatesCityFrom = function () {
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -42,17 +24,15 @@ class TimeZoneConverter {
                 "cache-control": "no-cache",
                 "postman-token": "c4631188-a0f2-59e4-6cda-bc789b657fca"
             }
-        }
-
-        $.ajax(settings).done(function (response): void {
+        };
+        $.ajax(settings).done(function (response) {
             //will set lat and long variables, and then call next ajax call in the chain
             this.cityFromLat = String(response[1].geometry.location.lat);
             this.cityFromLong = String(response[1].geometry.location.lng);
             this.getcoordinatesCityTo();
         });
-    }
-
-    getcoordinatesCityTo(): void {
+    };
+    TimeZoneConverter.prototype.getcoordinatesCityTo = function () {
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -62,17 +42,16 @@ class TimeZoneConverter {
                 "cache-control": "no-cache",
                 "postman-token": "c4631188-a0f2-59e4-6cda-bc789b657fca"
             }
-        }
-
-        $.ajax(settings).done(function (response): void {
+        };
+        $.ajax(settings).done(function (response) {
             //will set lat and long variables, and then call next ajax call in the chain
             this.cityToLat = String(response[1].geometry.location.lat);
             this.cityToLong = String(response[1].geometry.location.lng);
             this.getTimeZoneCityFrom();
         });
-    }
+    };
     //these methods cannot be executed concurrently due to calculation coming immediately after, will need to chain two requests
-    getTimeZoneCityFrom(): void {
+    TimeZoneConverter.prototype.getTimeZoneCityFrom = function () {
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -82,16 +61,13 @@ class TimeZoneConverter {
                 "cache-control": "no-cache",
                 "postman-token": "10e676fd-2038-9a90-8fde-596b448b8b63"
             }
-        }
-
-        $.ajax(settings).done(function (response): void {
+        };
+        $.ajax(settings).done(function (response) {
             this.cityFromTimezone = String(response.zoneName);
             this.getTimeZoneCityTo();
         });
-
-    }
-
-    getTimeZoneCityTo(): void {
+    };
+    TimeZoneConverter.prototype.getTimeZoneCityTo = function () {
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -101,15 +77,13 @@ class TimeZoneConverter {
                 "cache-control": "no-cache",
                 "postman-token": "10e676fd-2038-9a90-8fde-596b448b8b63"
             }
-        }
-
+        };
         $.ajax(settings).done(function (response) {
             this.cityToTimezone = String(response.zoneName);
             this.convertTime();
         });
-    }
-
-    convertTime(): void {
+    };
+    TimeZoneConverter.prototype.convertTime = function () {
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -119,23 +93,17 @@ class TimeZoneConverter {
                 "cache-control": "no-cache",
                 "postman-token": "5311186b-b1aa-099a-5ac8-d4778e9ff6ba"
             }
-        }
-
-        $.ajax(settings).done(function (response): void {
+        };
+        $.ajax(settings).done(function (response) {
             $("#resultBtn").text(response);
         });
-    }
-}
-
-function convertTime(): void{
+    };
+    return TimeZoneConverter;
+}());
+function convertTime() {
     var cityFrom = $("#cityfrom").val();
-    var cityTo = $("#cityto").val()
-
+    var cityTo = $("#cityto").val();
     var timeZoneConverter = new TimeZoneConverter();
     timeZoneConverter.setCities(cityFrom, cityTo);
     timeZoneConverter.retrieveInformation();
-    
-
-
-
 }
